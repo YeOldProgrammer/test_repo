435c9ff2c5eb86af74347dc90a3934cde0a5812d
YeOldProgrammer <github@olker.us>
2021-10-04 08:14:37 -0500

    Test Repo

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     author_email='tim@olker.us',
     # Needed to actually package something
     packages=['test_repo'],
+    entry_points={'console_scripts': ['test_repo = test_repo.file1:hw']},
     # Needed for dependencies
     install_requires=['requests'],
     version='0.2',
